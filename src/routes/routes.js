0a33004d10fa77408d9396b7c8eadd409421fad2
Davi Silva <davisilvaphoto@gmail.com>
2022-01-28 20:32:05 -0300

    fix route

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { ProductController } from "../controller/product-controller.js";
+import { ProductsController } from "../controller/product-controller.js";
 
 const routes = Router();
 
-routes.get("/products/:id?", new ProductController().get);
-routes.post("/products", new ProductController().post);
-routes.put("/products/:id", new ProductController().put);
-routes.delete("/products/:id", new ProductController().remove);
+routes.get("/products/:id?", new ProductsController().get);
+routes.post("/products", new ProductsController().post);
+routes.put("/products/:id", new ProductsController().put);
+routes.delete("/products/:id", new ProductsController().remove);
 
 export { routes };
