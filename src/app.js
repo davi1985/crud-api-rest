2588323007140f8c371823f01cf0263636baffa8
Davi Silva <davisilvaphoto@gmail.com>
2022-01-31 09:04:39 -0300

    refactor app.js

---
+++ src/app.js
@@ -0,0 +1,31 @@
+import express from "express";
+import cors from "cors";
+
+import { routes } from "./routes/routes.js";
+import { connect } from "./database/db.js";
+
+const app = express();
+
+connect();
+
+const allowedOrigins = ["http://127.0.0.1:5500"];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      let allowed = true;
+
+      if (!origin) allowed = true;
+
+      if (!allowedOrigins.includes(origin)) allowed = false;
+
+      callback(null, allowed);
+    },
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api", routes);
+
+export { app };
--- src/server.js
+++ src/server.js
@@ -1,32 +1,4 @@
-import express from "express";
-import cors from "cors";
-
-import { routes } from "./routes/routes.js";
-import { connect } from "./database/db.js";
-
-const app = express();
-
-connect();
-
-const allowedOrigins = ["http://127.0.0.1:5500"];
-
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      let allowed = true;
-
-      if (!origin) allowed = true;
-
-      if (!allowedOrigins.includes(origin)) allowed = false;
-
-      callback(null, allowed);
-    },
-  })
-);
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use("/api", routes);
+import { app } from "./app.js";
 
 const port = process.env.PORT || 3333;
 
